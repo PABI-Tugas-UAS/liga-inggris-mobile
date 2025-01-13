c56c7b57d5a6cc7072a6aeb75389beb21ffa07ac
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-01-13 18:31:29 +0700

    add: no timeline match text for upcoming match

--- lib/app/utils/get_percentage_value.dart
+++ lib/app/utils/get_percentage_value.dart
@@ -1,4 +1,7 @@
 List<double> calculatePercentage(num x, num y) {
+  if (x == 0 && y == 0) {
+    return [50, 50];
+  }
   num total = x + y;
   double xPercentage = (x / total) * 100;
   double yPercentage = (y / total) * 100;
